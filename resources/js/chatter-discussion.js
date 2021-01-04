1a3b14fd1740813eaa7a81f103cf905823af1b1a
Patrick Henninger <patrick.henninger@contagt.com>
2021-01-04 19:22:38 +0100

    try to fix EasyMDE import,

--- resources/js/chatter-discussion.js
+++ resources/js/chatter-discussion.js
@@ -1,4 +1,4 @@
-import 'easymde';
+import * as EasyMDE from 'easymde';
 
 document.addEventListener("DOMContentLoaded", function() { 
     /* Initialize new response editor */
--- resources/js/chatter-home.js
+++ resources/js/chatter-home.js
@@ -1,4 +1,4 @@
-import 'easymde';
+import * as EasyMDE from 'easymde';
 
 document.addEventListener("DOMContentLoaded", function() {
     new EasyMDE({element: document.getElementById('new-discussion-textarea')});
